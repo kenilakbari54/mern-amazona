d503b2d3c4a923561a85e991442d8515d084a641
Kenil Akbari <89933076+kenilakbari54@users.noreply.github.com>
2023-03-01 22:49:52 +0530

    Update Product.js

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -17,7 +17,7 @@ function Product(props) {
   const addToCartHandler = async (item) => {
     const existItem = cartItems.find((x) => x._id === product._id);
     const quantity = existItem ? existItem.quantity + 1 : 1;
-    const { data } = await axios.get(`/api/products/${item._id}`);
+    const { data } = await axios.get(`https://amazon-clone-zjrx.onrender.com/api/products/${item._id}`);
     if (data.countInStock < quantity) {
       window.alert('Sorry. Product is out of stock');
       return;
@@ -30,11 +30,11 @@ function Product(props) {
 
   return (
     <Card>
-      <Link to={`/product/${product.slug}`}>
+      <Link to={`https://amazon-clone-zjrx.onrender.com/product/${product.slug}`}>
         <img src={product.image} className="card-img-top" alt={product.name} />
       </Link>
       <Card.Body>
-        <Link to={`/product/${product.slug}`}>
+        <Link to={`https://amazon-clone-zjrx.onrender.com/product/${product.slug}`}>
           <Card.Title>{product.name}</Card.Title>
         </Link>
         <Rating rating={product.rating} numReviews={product.numReviews} />
