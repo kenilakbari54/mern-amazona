7f43ca9c251833021f22596eb72622ec09d93d08
Kenil Akbari <89933076+kenilakbari54@users.noreply.github.com>
2023-03-02 09:57:01 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -60,4 +60,7 @@ app.get("/",(req,res)=>{
   res.setHeader("Access-Control-Allow-Credentials","true");
   res.send("APi is running...");
 });
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE'); I
+header('Access-Control-Allow-Headers: Content-Type, X-Auth-T en, Origin, Authorization');
 
